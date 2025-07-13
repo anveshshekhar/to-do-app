189312b8eac43ae2c0c90eb6e45c626bc99c87a0
Anvesh Shekhar <72605577+anveshshekhar@users.noreply.github.com>
2025-07-13 17:54:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="60 Day Roadmap",layout="wide")
 def display_roadmap():
     st.title("📈 60-Day Intensive Tech Mastery Roadmap")
     st.markdown("""
